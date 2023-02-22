c40bb9d3187ae65b60c36de1b6cb8588c8506e5f
Ashwin Kadam <53835307+ashwinkadam@users.noreply.github.com>
2023-02-21 22:23:42 -0500

    Update Assignment_2.py

--- Assignment_2/Assignment_2.py
+++ Assignment_2/Assignment_2.py
@@ -9,57 +9,57 @@ import warnings
 import os
 warnings.filterwarnings('ignore')
 import streamlit as st
-from snowflake.snowpark.session import Session
+# from snowflake.snowpark.session import Session
 # from snowflake.snowpark.functions import avg, sum, col,lit
 from snowflake.connector import connect
 
 st.set_page_config(page_title='Sprocket Bikes', page_icon=':bike:')
 
 
-account = os.environ.get('account')
-user = os.environ.get('user')
-password = os.environ.get('password')
-role = os.environ.get('role')
-warehouse = os.environ.get('warehouse')
-database = os.environ.get('database')
-schema = os.environ.get('schema')
+# account = os.environ.get('account')
+# user = os.environ.get('user')
+# password = os.environ.get('password')
+# role = os.environ.get('role')
+# warehouse = os.environ.get('warehouse')
+# database = os.environ.get('database')
+# schema = os.environ.get('schema')
 
 
 
 #connection to snowflake
 # Define the connection parameters
-# connection_parameters = {
-#     "account": "ca60211.us-east4.gcp",
-#     "user": "PYASHISHMHATRE",
-#     "password": "!Ashish123",
-#     "role": "SYSADMIN",
-#     "warehouse": "COMPUTE_WH",
-#     "database": "SPROCKET",
-#     "schema": "PUBLIC"
-# }
-
-
-
-
-
 connection_parameters = {
-    "account": os.environ.get("SNOWFLAKE_ACCOUNT"),
-    "user": os.environ.get("SNOWFLAKE_USER"),
-    "password": os.environ.get("SNOWFLAKE_PASSWORD"),
-    "role": os.environ.get("SNOWFLAKE_ROLE"),
-    "warehouse": os.environ.get("SNOWFLAKE_WAREHOUSE"),
-    "database": os.environ.get("SNOWFLAKE_DATABASE"),
-    "schema": os.environ.get("SNOWFLAKE_SCHEMA")
+    "account": "ca60211.us-east4.gcp",
+    "user": "PYASHISHMHATRE",
+    "password": "!Ashish123",
+    "role": "SYSADMIN",
+    "warehouse": "COMPUTE_WH",
+    "database": "SPROCKET",
+    "schema": "PUBLIC"
 }
 
 
-SNOWFLAKE_ACCOUNT = "ca60211.us-east4.gcp"
-SNOWFLAKE_USER = "PYASHISHMHATRE"
-SNOWFLAKE_PASSWORD = "!Ashish123"
-SNOWFLAKE_ROLE = "SYSADMIN"
-SNOWFLAKE_WAREHOUSE = "COMPUTE_WH"
-SNOWFLAKE_DATABASE = "SPROCKET"
-SNOWFLAKE_SCHEMA = "PUBLIC"
+
+
+
+# connection_parameters = {
+#     "account": os.environ.get("SNOWFLAKE_ACCOUNT"),
+#     "user": os.environ.get("SNOWFLAKE_USER"),
+#     "password": os.environ.get("SNOWFLAKE_PASSWORD"),
+#     "role": os.environ.get("SNOWFLAKE_ROLE"),
+#     "warehouse": os.environ.get("SNOWFLAKE_WAREHOUSE"),
+#     "database": os.environ.get("SNOWFLAKE_DATABASE"),
+#     "schema": os.environ.get("SNOWFLAKE_SCHEMA")
+# }
+
+
+# SNOWFLAKE_ACCOUNT = "ca60211.us-east4.gcp"
+# SNOWFLAKE_USER = "PYASHISHMHATRE"
+# SNOWFLAKE_PASSWORD = "!Ashish123"
+# SNOWFLAKE_ROLE = "SYSADMIN"
+# SNOWFLAKE_WAREHOUSE = "COMPUTE_WH"
+# SNOWFLAKE_DATABASE = "SPROCKET"
+# SNOWFLAKE_SCHEMA = "PUBLIC"
 
 
 
